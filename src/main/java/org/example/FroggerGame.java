a7d3591c68f564135fce4592d524dac2bdf6f983
mahsa <mahsa_k87@icloud.com>
2023-02-01 15:38:40 +0100

    Mahsa

--- src/main/java/org/example/FroggerGame.java
+++ src/main/java/org/example/FroggerGame.java
@@ -1,9 +1,8 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.Random;
+
 public class FroggerGame {
     private int ticks = 30;
     private Screen screen;
-    public FroggerGame(Screen screen)  {
-        this.screen = screen;
-
-    }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,10 @@ package org.example;
 
 public class Main {
     static Screen screen = new Screen();
-    static FroggerGame froggerGame = new FroggerGame(screen);
+    static FroggerGame froggerGame = new FroggerGame();
+
     public static void main(String[] args) {
         screen.border();
-
-
     }
+
 }
