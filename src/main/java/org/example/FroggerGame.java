8fd899add5b7fc3b32c39cb8c690da4a21c0406f
David Mortensen <davidmortensen90@gmail.com>
2023-02-01 16:14:46 +0100

    FLERA BILAR

--- src/main/java/org/example/FroggerGame.java
+++ src/main/java/org/example/FroggerGame.java
@@ -13,7 +13,27 @@ public class FroggerGame {
 
     }
     public void addCars() {
-        car.add(new Position(33, 7));
-        screen.putChar(car.get(0).col, car.get(0).row, '\u2588', Screen.GREEN, Screen.GREEN);
+        for (int i = 0; i < 6; i++) {
+            car.add(new Position(2 + (i * 13), 9));
+            screen.putChar(car.get(i).col, car.get(i).row, '\u2588', Screen.GREEN, Screen.GREEN);
+            screen.putChar(car.get(i).col + 1, car.get(i).row, '\u2588', Screen.BLUE, Screen.GREEN);
+            screen.putChar(car.get(i).col + 2, car.get(i).row, '\u2588', Screen.GREEN, Screen.GREEN);
+            screen.putChar(car.get(i).col + 3, car.get(i).row, '\u2588', Screen.GREEN, Screen.GREEN);
+        }
+        for (int i = 0; i < 6; i++) {
+            car.add(new Position(2 + (i * 13), 7));
+            screen.putChar(car.get(i + 5).col, car.get(i + 5).row, '\u2588', Screen.GREEN, Screen.GREEN);
+            screen.putChar(car.get(i + 5).col + 1, car.get(i + 5).row, '\u2588', Screen.BLUE, Screen.GREEN);
+            screen.putChar(car.get(i + 5).col + 2, car.get(i + 5).row, '\u2588', Screen.GREEN, Screen.GREEN);
+            screen.putChar(car.get(i + 5).col + 3, car.get(i + 5).row, '\u2588', Screen.GREEN, Screen.GREEN);
+        }
+        for (int i = 0; i < 6; i++) {
+            car.add(new Position(2 + (i * 13), 5));
+            screen.putChar(car.get(i).col, car.get(i).row, '\u2588', Screen.GREEN, Screen.GREEN);
+            screen.putChar(car.get(i).col + 1, car.get(i).row, '\u2588', Screen.BLUE, Screen.GREEN);
+            screen.putChar(car.get(i).col + 2, car.get(i).row, '\u2588', Screen.GREEN, Screen.GREEN);
+            screen.putChar(car.get(i).col + 3, car.get(i).row, '\u2588', Screen.GREEN, Screen.GREEN);
+        }
+
     }
 }
