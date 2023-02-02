e1ed5ec04af4f7d023b1bc3de0b683144ed803e0
David Mortensen <davidmortensen90@gmail.com>
2023-02-02 11:41:07 +0100

    Changed car arraylists to one arraylist, added moveCars() method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,16 @@ import java.util.ArrayList;
 
 public class Main {
     static Screen screen = new Screen();
-    static FroggerGame froggerGame = new FroggerGame(screen);
+    static FroggerGame froggerGame;
+
+    static {
+        try {
+            froggerGame = new FroggerGame(screen);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String[] args) {
         screen.border();
 
