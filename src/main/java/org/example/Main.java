5928964af643e7ba5c7ed8fdd912f8390c69491f
mahsa <mahsa_k87@icloud.com>
2023-02-02 10:39:45 +0100

    Mahsa color

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Main {
     static Screen screen = new Screen();
-    static FroggerGame froggerGame = new FroggerGame();
+    static FroggerGame froggerGame = new FroggerGame(screen);
 
     public static void main(String[] args) {
         screen.border();
